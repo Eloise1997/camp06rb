dcb7806582dfadbf982f93605d394608d87ee07e
Eloise <drabbit0606@gmail.com>
2023-09-25 00:20:51 +0800

    0925 MyOrder資料從db.order撈 Cart => Order

---
+++ FProjectCamping/Controllers/Cart/CartsController.cs
@@ -0,0 +1,120 @@
+﻿using FProjectCamping.Models.Services;
+using FProjectCamping.Models.ViewModels.Carts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ApplicationServices;
+using System.Web.Mvc;
+
+namespace FProjectCamping.Controllers.Cart
+{
+	public class CartsController : Controller
+	{
+		private readonly Models.Services.ProfileService _profileService = new Models.Services.ProfileService();
+		private readonly CartService _cartService = new CartService();
+
+		// GET: Cart
+		public ActionResult Cart()
+		{
+			// memberId 也許是從登入資訊來
+			var model = _cartService.Get("aliee");
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult Cart(CartVm vm)
+		{
+			_cartService.Update(vm);
+
+			return RedirectToAction("OrderInfo");
+		}
+
+		public ActionResult OrderInfo()
+		{
+			var cart = _cartService.Get("aliee");
+
+			// 預設帶入 訂房人資料
+			cart.ContactProfile = _profileService.GetMemberProfile("aliee");
+			return View(cart);
+		}
+
+		[HttpPost]
+		public ActionResult OrderInfo(CartVm vm)
+		{
+			if (!ModelState.IsValid) return View(vm);
+
+			//var buyer = User.Identity.Name;
+			var buyer = "aliee";
+			var cart = _cartService.GetOrCreateCart(buyer);
+			cart.AllowCheckout = cart.Items.Any();
+
+			if (cart.AllowCheckout == false)
+			{
+				ModelState.AddModelError("", "購物車是空的,無法進行結帳");
+				return View(vm);
+			}
+
+			cart.ContactProfile = vm.ContactProfile;
+			cart.Coupon = vm.Coupon;
+			cart.PaymentType = vm.PaymentType;
+
+			_cartService.ProcessCheckout(buyer, cart);
+			return RedirectToAction("Pay", "Orders"); // todo 轉導到結帳頁
+		}
+
+		/// <summary>
+		/// 將 CartItem加入購物車
+		/// </summary>
+		/// <param name="vm"></param>
+		/// <returns></returns>
+		[Authorize]
+		[HttpPost]
+		public string AddItem(CartItemsVm vm)
+		{
+			string buyer = User.Identity.Name; // 買家帳號
+			var result = _cartService.AddToCart(buyer, vm); //加入購物車
+
+			return result; //沒傳回任何東西
+		}
+
+		[Authorize]
+		public ActionResult Checkout()
+		{
+			var buyer = User.Identity.Name;
+			var cart = _cartService.GetOrCreateCart(buyer);
+
+			if (cart.AllowCheckout == false) ViewBag.ErrorMessage = "購物車是空的,無法進行結帳";
+
+			return View();
+		}
+
+		[Authorize]
+		[HttpPost]
+		public ActionResult Checkout(CheckoutVm vm)
+		{
+			if (!ModelState.IsValid) return View(vm);
+
+			var cartService = _cartService;
+
+			var buyer = User.Identity.Name;
+			var cart = cartService.GetOrCreateCart(buyer);
+
+			if (cart.AllowCheckout == false)
+			{
+				ModelState.AddModelError("", "購物車是空的,無法進行結帳");
+				return View(vm);
+			}
+
+			//cartService.ProcessCheckout(buyer, cart, vm);
+			return View("ConfirmCheckout"); // todo 轉導到結帳頁
+		}
+
+		public ActionResult DeleteCartItem(int cartItemId)
+		{
+			_cartService.DeleteCartItem(cartItemId);
+			return new EmptyResult();
+		}
+	}
+}
---
+++ FProjectCamping/Models/Services/OrderService.cs
@@ -0,0 +1,63 @@
+﻿using FProjectCamping.Models.EFModels;
+using FProjectCamping.Models.Respositories;
+using FProjectCamping.Models.ViewModels.Carts;
+using FProjectCamping.Models.ViewModels.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static FProjectCamping.MvcApplication;
+
+namespace FProjectCamping.Models.Services
+{
+	public class OrderService
+	{
+		private readonly OrderRepository _orderRepository = new OrderRepository();
+		private readonly PaymentTypeRepository _paymentTypeRepository = new PaymentTypeRepository();
+
+		public void CreateOrder(string account, CartVm cart)
+		{
+			// todo : 拉到Repo
+			var db = new AppDbContext();
+			var memberId = db.Members.First(m => m.Account == account).Id;
+
+			var order = new Order
+			{
+				MemberId = memberId,
+				Name = cart.ContactProfile.Name,
+				PhoneNum = cart.ContactProfile.PhoneNum,
+				Email = cart.ContactProfile.Email,
+				OrderTime = DateTime.Now,
+				Coupon = cart.Coupon,
+				Status = 1, // todo : 建立enum
+				PaymentTypeId = cart.PaymentType,
+				TotalPrice = cart.TotalPrice,
+			};
+			// 新增訂單明細
+			foreach (var item in cart.Items)
+			{
+				var orderItem = new OrderItem
+				{
+					RoomId = item.RoomId,
+					Days = item.Days,
+					CheckInDate = Convert.ToDateTime(item.CheckInDate),
+					CheckOutDate = Convert.ToDateTime(item.CheckOutDate),
+					ExtraBed = item.ExtraBed,
+					ExtraBedPrice = item.ExtraBedPrice,
+					SubTotal = item.SubTotal
+				};
+				order.OrderItems.Add(orderItem);
+			}
+			db.Orders.Add(order);
+			db.SaveChanges();
+		}
+
+		public PayOrderVm GetOrder(int orderId)
+		{
+			var order = _orderRepository.GetOrders(orderId);
+			var vm = AutoMapperHelper.MapperObj.Map<PayOrderVm>(order);
+			vm.PaymentType = _paymentTypeRepository.GetTypeName(order.PaymentTypeId).Name;
+			return vm;
+		}
+	}
+}
