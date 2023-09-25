790abeee157c4b1981a40c0880fda3d4c40b9750
Randy <weton0110@gmail.com>
2023-09-26 01:03:19 +0800

    安裝paypal

--- FProjectCamping/Controllers/Orders/OrdersController.cs
+++ FProjectCamping/Controllers/Orders/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using FProjectCamping.Models.Services;
 using FProjectCamping.Models.ViewModels.Orders;
+using PayPal.Api;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,118 +9,160 @@ using System.Web.Mvc;
 
 namespace FProjectCamping.Controllers
 {
-	public class OrdersController : Controller
-	{
-		private readonly OrderService _orderService = new OrderService();
+    public class OrdersController : Controller
+    {
+        private readonly OrderService _orderService = new OrderService();
 
-		// GET: Orders
-		public ActionResult Index()
-		{
-			return View();
-		}
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		//public ActionResult GetByMember()
-		//{
-		//#region Fake Data
+        //public ActionResult GetByMember()
+        //{
+        //#region Fake Data
 
-		//var model = new List<GetByMemberVm>
-		//{
-		//	new GetByMemberVm()
-		//	{
-		//		DisplayNumber = 1,
-		//		OrderNumber = "AA0915230001",
-		//		OrderTime = "2023/09/15",
-		//		PaymentType = "現金",
-		//		Price = 36000,
-		//		Status = "已完成",
-		//		OrderItems = new List<OrderItems>
-		//		{
-		//			new OrderItems
-		//			{
-		//				RoomType = "森林雙人房",
-		//				CheckInDate = "2023/09/15",
-		//				CheckOutDate = "2023/09/17",
-		//				Days = 2,
-		//				SubTotal = 1800,
-		//			},
-		//			new OrderItems
-		//			{
-		//				RoomType = "森林四人房",
-		//				CheckInDate = "2023/09/15",
-		//				CheckOutDate = "2023/09/17",
-		//				Days = 2,
-		//				SubTotal = 3500,
-		//			}
-		//		}
-		//	},
-		//	new GetByMemberVm()
-		//	{
-		//		DisplayNumber = 2,
-		//		OrderNumber = "AA0915230002",
-		//		OrderTime = "2023/09/17",
-		//		PaymentType = "現金",
-		//		Price = 555,
-		//		Status = "已完成",
-		//		OrderItems = new List<OrderItems>
-		//		{
-		//			new OrderItems
-		//			{
-		//				RoomType = "森林雙人房",
-		//				CheckInDate = "2023/09/15",
-		//				CheckOutDate = "2023/09/17",
-		//				Days = 2,
-		//				SubTotal = 1800,
-		//			},
-		//			new OrderItems
-		//			{
-		//				RoomType = "森林四人房",
-		//				CheckInDate = "2023/09/15",
-		//				CheckOutDate = "2023/09/17",
-		//				Days = 2,
-		//				SubTotal = 3500,
-		//			}
-		//		}
-		//	},
-		//	new GetByMemberVm()
-		//	{
-		//		DisplayNumber = 3,
-		//		OrderNumber = "AA0915230003",
-		//		OrderTime = "2023/09/19",
-		//		PaymentType = "現金",
-		//		Price = 3600,
-		//		Status = "已完成",
-		//		OrderItems = new List<OrderItems>
-		//		{
-		//			new OrderItems
-		//			{
-		//				RoomType = "森林雙人房",
-		//				CheckInDate = "2023/09/15",
-		//				CheckOutDate = "2023/09/17",
-		//				Days = 2,
-		//				SubTotal = 1800,
-		//			},
-		//			new OrderItems
-		//			{
-		//				RoomType = "森林四人房",
-		//				CheckInDate = "2023/09/15",
-		//				CheckOutDate = "2023/09/17",
-		//				Days = 2,
-		//				SubTotal = 3500,
-		//			}
-		//		}
-		//	}
-		//};
+        //var model = new List<GetByMemberVm>
+        //{
+        //	new GetByMemberVm()
+        //	{
+        //		DisplayNumber = 1,
+        //		OrderNumber = "AA0915230001",
+        //		OrderTime = "2023/09/15",
+        //		PaymentType = "現金",
+        //		Price = 36000,
+        //		Status = "已完成",
+        //		OrderItems = new List<OrderItems>
+        //		{
+        //			new OrderItems
+        //			{
+        //				RoomType = "森林雙人房",
+        //				CheckInDate = "2023/09/15",
+        //				CheckOutDate = "2023/09/17",
+        //				Days = 2,
+        //				SubTotal = 1800,
+        //			},
+        //			new OrderItems
+        //			{
+        //				RoomType = "森林四人房",
+        //				CheckInDate = "2023/09/15",
+        //				CheckOutDate = "2023/09/17",
+        //				Days = 2,
+        //				SubTotal = 3500,
+        //			}
+        //		}
+        //	},
+        //	new GetByMemberVm()
+        //	{
+        //		DisplayNumber = 2,
+        //		OrderNumber = "AA0915230002",
+        //		OrderTime = "2023/09/17",
+        //		PaymentType = "現金",
+        //		Price = 555,
+        //		Status = "已完成",
+        //		OrderItems = new List<OrderItems>
+        //		{
+        //			new OrderItems
+        //			{
+        //				RoomType = "森林雙人房",
+        //				CheckInDate = "2023/09/15",
+        //				CheckOutDate = "2023/09/17",
+        //				Days = 2,
+        //				SubTotal = 1800,
+        //			},
+        //			new OrderItems
+        //			{
+        //				RoomType = "森林四人房",
+        //				CheckInDate = "2023/09/15",
+        //				CheckOutDate = "2023/09/17",
+        //				Days = 2,
+        //				SubTotal = 3500,
+        //			}
+        //		}
+        //	},
+        //	new GetByMemberVm()
+        //	{
+        //		DisplayNumber = 3,
+        //		OrderNumber = "AA0915230003",
+        //		OrderTime = "2023/09/19",
+        //		PaymentType = "現金",
+        //		Price = 3600,
+        //		Status = "已完成",
+        //		OrderItems = new List<OrderItems>
+        //		{
+        //			new OrderItems
+        //			{
+        //				RoomType = "森林雙人房",
+        //				CheckInDate = "2023/09/15",
+        //				CheckOutDate = "2023/09/17",
+        //				Days = 2,
+        //				SubTotal = 1800,
+        //			},
+        //			new OrderItems
+        //			{
+        //				RoomType = "森林四人房",
+        //				CheckInDate = "2023/09/15",
+        //				CheckOutDate = "2023/09/17",
+        //				Days = 2,
+        //				SubTotal = 3500,
+        //			}
+        //		}
+        //	}
+        //};
 
-		//#endregion Fake Data
+        //#endregion Fake Data
 
-		//return View(model);
-		//}
+        //return View(model);
+        //}
 
-		public ActionResult Pay()
-		{
-			int orderId = 5;
-			var model = _orderService.GetOrder(orderId);
-			return View(model);
-		}
-	}
+        public ActionResult Pay()
+        {
+            int orderId = 5;
+            var model = _orderService.GetOrder(orderId);
+            return View(model);
+        }
+
+        public ActionResult Paypal()
+        {
+            // 設定APIContext
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+
+            // 設定付款信息
+            var payment = Payment.Create(apiContext, new Payment
+            {
+                intent = "sale",
+                payer = new Payer { payment_method = "paypal" },
+                transactions = new List<Transaction>
+        {
+            new Transaction
+            {
+                description = "商品描述",
+                invoice_number = new Random().Next(100000).ToString(), // 生成一個隨機的發票號碼
+                amount = new Amount { currency = "USD", total = "20.00" }, // 設定金額和貨幣
+            }
+        },
+                redirect_urls = new RedirectUrls
+                {
+                    return_url = "http://localhost:yourport/Home/ExecutePayment", // 付款完成後返回的URL
+                    cancel_url = "http://localhost:yourport/Home/Payment" // 付款取消後返回的URL
+                }
+            });
+
+            // 取得並傳送PayPal付款頁面的鏈接到前端
+            var links = payment.links.GetEnumerator();
+            string paypalRedirectUrl = null;
+            while (links.MoveNext())
+            {
+                Links link = links.Current;
+                if (link.rel.ToLower().Trim().Equals("approval_url"))
+                {
+                    paypalRedirectUrl = link.href;
+                }
+            }
+            return Redirect(paypalRedirectUrl);
+        }
+    }
 }
--- FProjectCampingBackend/Controllers/OrdersController.cs
+++ FProjectCampingBackend/Controllers/OrdersController.cs
@@ -1,5 +1,7 @@
 ﻿using FProjectCampingBackend.Models.EFModels;
+using FProjectCampingBackend.Models.ViewModels.Orders;
 using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
@@ -7,127 +9,144 @@ using System.Web.Mvc;
 
 namespace FProjectCampingBackend.Controllers
 {
-	public class OrdersController : Controller
-	{
-		private AppDbContext db = new AppDbContext();
+    public class OrdersController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
 
-		// GET: Orders
-		public ActionResult Index(Order vm)
-		{
-			var orders = db.Orders.Include(o => o.Member).Include(o => o.PaymentType);
-			return View(orders.ToList());
-		}
+        // GET: Orders
+        public ActionResult Index(Order vm)
+        {
+            var orders = db.Orders.Include(o => o.Member).Include(o => o.PaymentType);
 
-		// GET: Orders/Details/5
-		public ActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Order order = db.Orders.Find(id);
-			if (order == null)
-			{
-				return HttpNotFound();
-			}
-			return View(order);
-		}
+            var resule = new List<OrderVm>();
+            foreach (var item in orders)
+            {
+                var orderVm = new OrderVm()
+                {
+                    //OrderNumber = vm.OrderNumber // 考慮是否要增加
+                    OrderTime = vm.OrderTime.ToShortDateString(),
+                    PaymentType = vm.PaymentType.Name,
+                    Price = vm.TotalPrice,
+                    Status = vm.Status,
+                    Name = vm.Name,
+                    Email = vm.Email,
+                    PhoneNum = vm.PhoneNum
+                };
+                resule.Add(orderVm);
+            }
+            return View(resule);
+        }
 
-		// GET: Orders/Create
-		public ActionResult Create()
-		{
-			ViewBag.MemberId = new SelectList(db.Members, "Id", "Account");
-			ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name");
-			return View();
-		}
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
 
-		// POST: Orders/Create
-		// 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-		// 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "Id,MemberId,OrderTime,Status,PaymentTypeId,TotalPrice")] Order order)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Orders.Add(order);
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Account");
+            ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name");
+            return View();
+        }
 
-			ViewBag.MemberId = new SelectList(db.Members, "Id", "Account", order.MemberId);
-			ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name", order.PaymentTypeId);
-			return View(order);
-		}
+        // POST: Orders/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MemberId,OrderTime,Status,PaymentTypeId,TotalPrice")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-		// GET: Orders/Edit/5
-		public ActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Order order = db.Orders.Find(id);
-			if (order == null)
-			{
-				return HttpNotFound();
-			}
-			ViewBag.MemberId = new SelectList(db.Members, "Id", "Account", order.MemberId);
-			ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name", order.PaymentTypeId);
-			return View(order);
-		}
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Account", order.MemberId);
+            ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name", order.PaymentTypeId);
+            return View(order);
+        }
 
-		// POST: Orders/Edit/5
-		// 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-		// 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "Id,MemberId,OrderTime,Status,PaymentTypeId,TotalPrice")] Order order)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Entry(order).State = EntityState.Modified;
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			ViewBag.MemberId = new SelectList(db.Members, "Id", "Account", order.MemberId);
-			ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name", order.PaymentTypeId);
-			return View(order);
-		}
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Account", order.MemberId);
+            ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name", order.PaymentTypeId);
+            return View(order);
+        }
 
-		// GET: Orders/Delete/5
-		public ActionResult Delete(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Order order = db.Orders.Find(id);
-			if (order == null)
-			{
-				return HttpNotFound();
-			}
-			return View(order);
-		}
+        // POST: Orders/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MemberId,OrderTime,Status,PaymentTypeId,TotalPrice")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Account", order.MemberId);
+            ViewBag.PaymentTypeId = new SelectList(db.PaymentTypes, "Id", "Name", order.PaymentTypeId);
+            return View(order);
+        }
 
-		// POST: Orders/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public ActionResult DeleteConfirmed(int id)
-		{
-			Order order = db.Orders.Find(id);
-			db.Orders.Remove(order);
-			db.SaveChanges();
-			return RedirectToAction("Index");
-		}
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				db.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-	}
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
